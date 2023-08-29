d8323e881ab4acb6b4d33275ec36ac97df460e5f
Fendy <zf@fendy5.cn>
2023-08-29 11:26:43 +0800

    fix: 修复api前缀不正确的问题

--- apis/user.ts
+++ apis/user.ts
@@ -1,8 +1,8 @@
 import { request } from '@/lib/request'
 import { UserInfoProp } from '@/store/userSlice'
 
-export const loginApi = () => request.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/getUserInfo`)
+export const loginApi = () => request.get(`/api/v1/getUserInfo`)
 
-export const getUserInfoApi = (params: { code: string }) => request.get<UserInfoProp>(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/getUserInfo`, { params } )
+export const getUserInfoApi = (params: { code: string }) => request.get<UserInfoProp>(`/api/v1/getUserInfo`, { params } )
 
-export const getTokenApi = (params: { code: string }) => request.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/login`, { params })
+export const getTokenApi = (params: { code: string }) => request.get(`/api/v1/login`, { params })
